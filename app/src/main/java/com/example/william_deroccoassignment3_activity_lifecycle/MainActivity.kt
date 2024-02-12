fb99754406a7d3592ada67eee0fd56a3440fd1af
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-02-12 13:11:05 -0500

    added logcat message for onStart and onResume (and added comments)

--- app/src/main/java/com/example/william_deroccoassignment3_activity_lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/william_deroccoassignment3_activity_lifecycle/MainActivity.kt
@@ -5,9 +5,27 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    // This method is called when the activity is first created.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "onCreate() called")
+        Log.d(TAG, "onCreate() called")
+    }
+
+    // This method is called when the activity becomes visible to the user but is not yet in the foreground.
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    // This method is called when the activity is visible and in the foreground.
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
     }
 }
